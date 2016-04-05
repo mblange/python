80eb9a8846f5da9a71cd966f9185e854cf90e3c8
mblange <mblange@users.noreply.github.com>
2016-04-04 21:13:29 -0500

    comprehensions?

--- jbossScanner.py
+++ jbossScanner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ## a tool for identifying vulnerable JBoss Application Servers.
 ## Author: Matt Lange
 ## Date: 3/31/2016
@@ -9,17 +10,15 @@ from libnmap.process import NmapProcess
 from libnmap.parser import NmapParser, NmapParserException
 import argparse
 
-__version__ = "0.1"
-
 RED = '\x1b[91m'
 GREEN = '\033[32m'
 ENDC = '\033[0m'
 
 # parse arguments
-ap = argparse.ArgumentParser(description='\n This is a jboss vulnerability scanner. You could also just use Metasploit... Version: v%s' % (__version__))
-ap.add_argument('target', type=str, help='CIDR network or ip address', default=None)
+ap = argparse.ArgumentParser(description='This is a jboss vulnerability scanner. You could also just use Metasploit...')
+ap.add_argument('target', type=str, help='CIDR notation or ip address', default=None)
 ap.add_argument('--ports', '-p', type=str, help='an nmap-stlye, comma-separated list of ports to scan', default='80,8080,8008,8000')
-ap.add_argument('--log', '-l',  help='write results to LOG', default='jBossScan.txt')
+ap.add_argument('--log', '-l',  help='write results to LOG', default='log_jBoss.txt')
 args = ap.parse_args()
 
 # validate target argument is a valid IP address
@@ -50,9 +49,12 @@ paths = ["/web-console/Invoker","/jmx-console/HtmlAdaptor?action=inspectMBean&na
 webServers = []
 
 #define a logging function
-def log(url):
+def log(url, status):
 	with open(args.log, 'a') as log:
-		log.write("[*] VULNERABLE [*]%s\n" %(url))
+		if status == 'good':
+			log.write(GREEN + "[*] OK [*] %s\n" %(url) + ENDC)
+		else:
+			log.write(RED + "[*] VULNERABLE [*] %s\n" %(url) + ENDC)
 
 # scan for open web servers
 def portScan():
@@ -62,12 +64,12 @@ def portScan():
 	if rc != 0:
 		print("nmap scan failed: {0}".format(nm.stderr))
 	parsed = NmapParser.parse(nm.stdout)
-	for host in parsed.hosts:
-		for service in host.services:
-			if service.open():
-				for path in paths:
-					webServers.append("%s:%s%s" % (host.address, service.port, path))
-	
+#	for host in parsed.hosts:
+#		for service in host.services:
+#			if service.open():
+#				for path in paths:
+#					webServers.append("%s:%s%s" % (host.address, service.port, path))
+	webServers = [str(host.address) + ':' + str(service.port) + str(path) for host in parsed.hosts for service in host.services if service.open() for path in paths ]
 # thread vulnCheck
 def vCheck():
 	Thread = threading.Thread
@@ -84,9 +86,10 @@ def vulnCheck(url):
 	status = r.status_code
 	if status == 200 or status == 500:
 		print(RED + "[*] VULNERABLE [*] %s" % url + ENDC)
-		log(url)
+		log(url, 'bad')
 	else:
 		print(GREEN + "[+] OK: %s" % url + ENDC)
+		log(url, 'good')
 
 def main():
 	portScan()
