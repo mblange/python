14f9f269f66df8a537071aa147a7a349108046b8
mblange <mblange@users.noreply.github.com>
2016-04-14 05:32:14 -0500

    added regex

--- ipSwap.py
+++ ipSwap.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
-import argv
+from sys import argv
 import re
 import random
 
-tor = []
-randomIP = []
+tor = [t.rstrip('\n') for t in open("tor.txt")]
+randomIP = [r.rstrip('\n') for r in open("random.txt")]
 
-with open(argv[1], "r") as fin
+with open(argv[1], "r") as fin:
 	for line in fin:
-		content = re.findall(r'172\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
+		content = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
 		if content[1] == '172.21.22.226':
 			#swap with tor	
 			re.sub('172\.21\.22\.226', random.choice(tor), line)
-		else:
+			print 'tor'
+		elif content[1] == '172.21.22.181':
 			#swap with randomIP
-			re.sub('172\.21\.22\.xxx', random.choice(randomIP), line)
+			re.sub('172\.21\.22\.181', random.choice(randomIP), line)
+			print 'random'
+		else:
+			print "nope"
+			continue
