f4dba5f528ae7594ac0f5bfceac010d22da74a65
Matthew Lange <matthew.lange4@gmail.com>
2016-05-09 14:27:22 -0500

    writes

--- ipSwap.py
+++ ipSwap.py
@@ -6,17 +6,22 @@ import random
 tor = [t.rstrip('\n') for t in open("tor.txt")]
 randomIP = [r.rstrip('\n') for r in open("random.txt")]
 
-with open(argv[1], "r") as fin:
-	for line in fin:
-		content = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
-		if content[1] == '172.21.22.226':
-			#swap with tor	
-			re.sub('172\.21\.22\.226', random.choice(tor), line)
-			print 'tor'
-		elif content[1] == '172.21.22.181':
-			#swap with randomIP
-			re.sub('172\.21\.22\.181', random.choice(randomIP), line)
-			print 'random'
-		else:
-			print "nope"
-			continue
+with open(argv[1], 'rb') as fin:
+	with open(argv[2], 'wb') as fout:
+		for line in fin:
+			line = line.strip()
+			matches = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
+			if matches:
+				match = matches[1]
+				if match == '172.21.22.226':
+					#swap with tor	
+					line = line.replace(match, random.choice(tor))
+					fout.write(line + "\n")
+				elif match == '172.21.22.181':
+					#swap with randomIP
+					line = line.replace(match, random.choice(randomIP))
+					fout.write(line + "\n")
+				else:
+					continue
+			else:
+				fout.write(line + "\n")
