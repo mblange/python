3e2912a163bda9b2f4e124732e7f595a99d48f65
Matthew Lange <matthew.lange4@gmail.com>
2016-05-23 09:06:05 -0500

    modified the requeses print statment for simplicity

--- start.py
+++ start.py
@@ -1,42 +1,40 @@
 #!/usr/bin/env python
 
+###################################
+# Template to thread http requests
+###################################
+
 from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
 import requests
 
-# Threading
 urls = [
   'http://www.python.org', 
   'http://www.python.org/about/',
-  'http://www.onlamp.com/pub/a/python/2003/04/17/metaclasses.html',
-  'http://www.python.org/doc/',
   'http://www.python.org/download/',
   'https://wiki.python.org/moin/',
   'http://planet.python.org/',
-  'https://wiki.python.org/moin/LocalUserGroups',
   'http://www.python.org/psf/',
   'http://www.python.org/community/awards/'
   ]
 
-# create function using requst module to GET 'urls'
+# create function using 'requsts' module to GET 'urls'
 def req(url):
 	r = requests.get(url)
 	return r
 
+############# Threading ######################
 # Make the Pool of workers
-pool = ThreadPool(4) # Sets the pool size to 4
+pool = ThreadPool(8) # Sets the pool size to 8
 
-# Open the urls in their own threads and return the results
+# Open the urls in their own threads and return list
 results = pool.map(req, urls)
 
 # Close the ppol and wait for the work to finish
 pool.close()
 pool.join()
+############# Threading ######################
 
-print results[0].status_code
-print results[0].headers
-print results[0].cookies
-print results[0].history
 print results[0].url
 for i in results:
-	print i.url
+	print i.url, i.status_code #.headers, .cookies, .history 
