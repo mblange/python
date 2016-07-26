8f3eab0529411189098b1eb3f23ef456b47b1b40
mblange <mblange@users.noreply.github.com>
2016-07-25 20:40:47 -0500

    added handler for port data and comments

--- nmapGetPorts.py
+++ nmapGetPorts.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 ###############################################################
-# parse nmap xml output and print comma separated list of ports
+# Parse nmap xml output and print comma separated list of ports
 # matt lange
 # 7/25/16
 ###############################################################
-## import stuff
 
+## import stuff
 import sys
 
 try:
@@ -17,15 +17,23 @@ except ImportError, e:
 	print "Exception: %s. Try 'pip install [module_name]'" % str(e)
 	sys.exit(1)
 
+# Accept argument containing an Nmap xml 'file' to parse
 ap = argparse.ArgumentParser(description='parser to retrieve ports from nmap scan')
 ap.add_argument('file', type=str, help='nmap.xml output file to parse', default=None)
 args = ap.parse_args()
 
+# Parse the file and create 'obj' handler
 obj = untangle.parse('%s' %(args.file))
 
+# Establish handler for all port data
+port_data = obj.nmaprun.host.ports.port
+
+# Establish empty list for storing ports
 ports = []
 
-for p in range(len(obj.nmaprun.host.ports.port)-1):
-	ports.append(int(obj.nmaprun.host.ports.port[p]['portid']))
+# Iterate through all ports and append them to the 'ports' list
+for p in range(len(port_data)):
+        ports.append(int(port_data[p]['portid']))
 
+# Print comma-separated list
 print ','.join(str(i) for i in ports)
