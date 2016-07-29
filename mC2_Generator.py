35601f9c32bf1550ad04e416c98e3df5ebc78e37
Matthew Lange <matthew.lange4@gmail.com>
2016-07-29 17:37:09 -0500

    added write to file

--- mC2_Generator.py
+++ mC2_Generator.py
@@ -2,8 +2,7 @@
 
 ###########################################################
 # parse OpenIOC file for IOCs to create CS MC2 Profile
-# basically just stolen (and modified) from:
-# http://www.jeffbryner.com/blog/itsec/pythoniocdump.html
+# Based on: http://www.jeffbryner.com/blog/itsec/pythoniocdump.html
 # Matt Lange
 # 7-29-2016
 ############################################################
@@ -27,16 +26,22 @@ root = ioco.getroot()
 
 print("Description:\n%s: %s"%(root.short_description, root.description))
 
-# Print IOCs we care about for Malleable C2 Profiles
-print "\nHere's the IOCs we probably care about for Malleable C2 Profile creation\n"
+# Print & store values of the IOC keys we care about for MC2 Profiles
+print "\nHere's the IOCs we probably care about for MC2 Profile creation\n"
 for ii in root.xpath("//*[local-name()='IndicatorItem']"):
 	if ii.Context.attrib.get("search") in iocs:
 		print('\t%s\t%s\t%s'%(ii.getparent().attrib.get("operator"), ii.Context.attrib.get("search"),ii.Content))
 		ioc_map[ii.Context.attrib.get("search")] = ii.Content
-		print ioc_map[ii.Context.attrib.get("search")]
 
 # Print all the IOCs
 print "\nHere's all the rest of the IOCs in this file\n"
 for ii in root.xpath("//*[local-name()='IndicatorItem']"):
 	if ii.Context.attrib.get("search") not in iocs:
 		print('\t%s\t%s\t%s'%(ii.getparent().attrib.get("operator"), ii.Context.attrib.get("search"),ii.Content))
+
+print ioc_map
+
+# TODO: write to CS MC2 Profile
+with open('new.profile', 'w') as file:
+	file.write(str(ioc_map))
+	
