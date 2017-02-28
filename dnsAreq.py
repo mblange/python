b36c72e038fc9b837e8605883e594a3384f2423b
mblange <mblange@users.noreply.github.com>
2017-02-27 21:08:43 -0600

    added optional request and domain arguments

--- dnsAreq.py
+++ dnsAreq.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+import sys
 import dns.resolver
 import os,binascii
 from time import sleep
+
+# Accept a number of requests to make if given, else, 100
+if len(sys.argv) > 1:
+    num = sys.argv[1]
+else:
+    num = 100
+
+# Accept a domain name, else, cyberpewpew.lol
+if len(sys.argv) > 2:
+    domain = sys.argv[2]
+else:
+    domain = 'cyberpewpew.lol'
 
 # Create new dns resolveer instance
 resolver = dns.resolver.Resolver()
@@ -10,18 +23,18 @@ resolver.timeout = .5
 resolver.lifetime = .5
 
 # Make 100 DNS 'A' record requests
-for d in range(0, 100):
-    domain = str()
+for d in range(0, int(num)):
+    rdomain = str()
     # Create two 16 byte hex subdomains
     sd1 = binascii.b2a_hex(os.urandom(16)) + '.'
     sd2 = binascii.b2a_hex(os.urandom(16)) + '.'
     # Prepend them to 'cyberpewpew.lol'
-    domain = sd1 + sd2 + 'cyberpewpew.lol'
+    rdomain = sd1 + sd2 + domain
     # Make DNS 'A' record request
     try:
         print 'attempt #:  %d' %d
-        print 'To: %s' %domain
-        ans = resolver.query(domain, "A")
+        print 'To: %s' %rdomain
+        ans = resolver.query(rdomain, "A")
         # Wait one second to mimic real requests timing
         sleep(.5)
     except Exception as e: 
