4009f5bb3db62e1fc7b4f3666a57c8606f8a0116
mblange <mblange@users.noreply.github.com>
2017-02-27 20:49:45 -0600

    fixed timeout and error messaging issues

--- dnsAreq.py
+++ dnsAreq.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import dns.resolver
 import os,binascii
+from time import sleep
 
 # Create new dns resolveer instance
-myRes = dns.resolver.Resolver()
+resolver = dns.resolver.Resolver()
+# All requested domains are bogus so don't wait around
+resolver.timeout = .5
+resolver.lifetime = .5
 
 # Make 100 DNS 'A' record requests
 for d in range(0, 100):
@@ -13,13 +17,12 @@ for d in range(0, 100):
     sd2 = binascii.b2a_hex(os.urandom(16)) + '.'
     # Prepend them to 'cyberpewpew.lol'
     domain = sd1 + sd2 + 'cyberpewpew.lol'
-    # For troubleshooting...
-    print d
-    print domain
-'''
     # Make DNS 'A' record request
     try:
-        myAns = myRes.query(domain, "A")
-    except:
-        continue
-'''
+        print 'attempt #:  %d' %d
+        print 'To: %s' %domain
+        ans = resolver.query(domain, "A")
+        # Wait one second to mimic real requests timing
+        sleep(.5)
+    except Exception as e: 
+        print str(e)
