ea1492d73037b813999e3c872d3b03b40117a22e
mblange <mblange@users.noreply.github.com>
2017-02-27 20:27:17 -0600

    cleaned up and added comments

--- dnsAreq.py
+++ dnsAreq.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
-# Create new resolveer instance
 import dns.resolver
-import random
-import string
 import os,binascii
 
-# Create new resolveer instance
+# Create new dns resolveer instance
 myRes = dns.resolver.Resolver()
 
+# Make 100 DNS 'A' record requests
 for d in range(0, 100):
     domain = str()
-    #sD1 = str()
-    #sD2 = str()
-    # Create random domain name
-    #for i in range(4):
-    #    s = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32)) + '.'
-    #    subDomain += s
-    #sD1 = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(16)) + '.'
-    #sD2 = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(162)) + '.'
+    # Create two 16 byte hex subdomains
     sd1 = binascii.b2a_hex(os.urandom(16)) + '.'
     sd2 = binascii.b2a_hex(os.urandom(16)) + '.'
+    # Prepend them to 'cyberpewpew.lol'
     domain = sd1 + sd2 + 'cyberpewpew.lol'
+    # For troubleshooting...
     print d
     print domain
 '''
+    # Make DNS 'A' record request
     try:
         myAns = myRes.query(domain, "A")
     except:
