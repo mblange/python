4ef6deceaf540e3938d5b4366316737dba6bb7c1
mblange <mblange@users.noreply.github.com>
2017-02-27 20:23:13 -0600

    changed to hex subdomains

--- dnsAreq.py
+++ dnsAreq.py
@@ -3,22 +3,29 @@
 import dns.resolver
 import random
 import string
+import os,binascii
 
 # Create new resolveer instance
 myRes = dns.resolver.Resolver()
 
 for d in range(0, 100):
     domain = str()
-    subDomain = str()
+    #sD1 = str()
+    #sD2 = str()
     # Create random domain name
-    for i in range(4):
-        s = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32)) + '.'
-        subDomain += s
-    domain = subDomain + 'cyberpewpew.lol'
+    #for i in range(4):
+    #    s = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32)) + '.'
+    #    subDomain += s
+    #sD1 = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(16)) + '.'
+    #sD2 = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(162)) + '.'
+    sd1 = binascii.b2a_hex(os.urandom(16)) + '.'
+    sd2 = binascii.b2a_hex(os.urandom(16)) + '.'
+    domain = sd1 + sd2 + 'cyberpewpew.lol'
     print d
     print domain
-
+'''
     try:
         myAns = myRes.query(domain, "A")
     except:
         continue
+'''
